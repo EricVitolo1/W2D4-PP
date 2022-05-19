406fcbc5e67d2c1100212bce571ecf79a15be0a6
Eric <eric@DESKTOP-HHJOMI5.localdomain>
2022-05-19 15:39:13 -0700

    10-11

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -9,14 +9,38 @@ console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 
 ***********************************************************************/
 
-// your code here!
+function interrupter(interruptingWord) {
+  return function(string) {
+    //interject the interruptingword between each word in the string
+    //create an empty array
+    //convert the string into an array
+    let words = string.split(' ');
+    //console.log(words);
+    //let newstring = "";
+    //iterate through the array , interjecting the interrupting word between each element
+    let newarray = [words[0]];
+
+    for (let i=1;i < words.length	; i++) {
+      let word = words[i];
+       newarray.push(interruptingWord);
+      newarray.push(word);
+
+    }
+
+    //convert the array back to the string
+     return newarray.join(' ');
+    // return the string
+  }
+
+}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
